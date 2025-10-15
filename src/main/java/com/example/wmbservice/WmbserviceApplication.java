efdbeb5a90d3f836417dcf29d29165c978781d8b
joshuarreid <2001josh@gmail.com>
2025-10-15 18:56:01 -0400

    Initial commit of Spring Boot app

---
+++ src/main/java/com/example/wmbservice/WmbserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.wmbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmbserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmbserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wmbservice/WmbserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wmbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmbserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
