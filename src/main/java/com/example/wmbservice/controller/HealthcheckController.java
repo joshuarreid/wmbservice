18de0d0acde948cc3a8cbe244ef35dd05461a3fe
joshuarreid <2001josh@gmail.com>
2025-11-07 21:48:35 -0500

    add health check endpoint

---
+++ src/main/java/com/example/wmbservice/controller/HealthcheckController.java
@@ -0,0 +1,13 @@
+package com.example.wmbservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthcheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
